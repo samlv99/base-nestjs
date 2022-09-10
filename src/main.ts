7bc97b0ee74410faa9e18cf75c1480f9d06c9350
samlv <samlv3112@gmail.com>
2022-09-10 22:40:03 +0700

    feature/connect-mysql

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const logger = new Logger('Main');
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get('port');
+  const appName = configService.get('appName');
+  const environment = configService.get('environment');
+
+  await app.listen(port);
+  logger.log(`Nest server started on port "${port}". Environment "${environment}"`);
+  logger.log(`---------------------------${appName}-----------------------------`);
 }
 bootstrap();
